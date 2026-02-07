7ebe791a0d51e9174d125b7cfde496fcbb4176e4
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-07 23:58:03 +0530

    update the format

--- scripts/firmware_test.py
+++ scripts/firmware_test.py
@@ -1,12 +1,14 @@
 import serial
 import time
 
-ser = serial.Serial('COM11', 9600, timeout=1)
+ser = serial.Serial("COM11", 9600, timeout=1)
 time.sleep(2)
+
 
 def send(cmd):
     ser.write((cmd + "\n").encode())
     print(">>", cmd)
+
 
 send("SET_PIN:4:OUTPUT")
 
