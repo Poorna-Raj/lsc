059f49e987f8137821f76c98b9c7fe68cc5d91c7
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-09 22:44:13 +0530

    define the LSC client

---
+++ lsc-client/src/LSCClient.ts
@@ -0,0 +1,58 @@
+import { SerialManager } from "./serial/SerialManager.js";
+import { LSCProtocol } from "./protocol/LSCProtocol.js";
+
+type EdgeHandler = (pin: number, type: "RISING" | "FALLING") => void;
+
+export class LSCClient {
+  private serial: SerialManager;
+  private edgeHandler: EdgeHandler[] = [];
+
+  constructor(port: string) {
+    this.serial = new SerialManager(port);
+  }
+
+  async connect() {
+    await this.serial.open(this.handleLine.bind(this));
+  }
+
+  private handleLine(line: string) {
+    if (line.startsWith("EDGE")) {
+      const [, type, pin] = line.split(":");
+      this.edgeHandler.forEach((h) => {
+        h(Number(pin), type as any);
+      });
+    }
+  }
+
+  onEdge(handler: EdgeHandler) {
+    this.edgeHandler.push(handler);
+  }
+
+  setPinMode(pin: number, mode: "INPUT" | "OUTPUT") {
+    this.serial.send(LSCProtocol.setPin(pin, mode));
+  }
+
+  writeDigital(pin: number, mode: "HIGH" | "LOW") {
+    this.serial.send(LSCProtocol.writeDigital(pin, mode));
+  }
+
+  readDigital(pin: number) {
+    this.serial.send(LSCProtocol.digitalRead(pin));
+  }
+
+  readAnalog(pin: number) {
+    this.serial.send(LSCProtocol.analogRead(pin));
+  }
+
+  writeAnalog(pin: number, value: number) {
+    this.serial.send(LSCProtocol.analogWrite(pin, value));
+  }
+
+  digitalWatch(pin: number) {
+    this.serial.send(LSCProtocol.watchDigital(pin));
+  }
+
+  digitalUnWatch(pin: number) {
+    this.serial.send(LSCProtocol.unWatchDigital(pin));
+  }
+}
