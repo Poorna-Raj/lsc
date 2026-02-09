f2aea1ebc1a4adb9ca78da422ebac9e793cd3329
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-09 22:49:07 +0530

    define the test script at index.ts

---
+++ lsc-client/src/index.ts
@@ -0,0 +1,17 @@
+import { LSCClient } from "./LSCClient.js";
+
+(async () => {
+  const lsc = new LSCClient("COM11");
+
+  await lsc.connect();
+
+  lsc.setPinMode(4, "OUTPUT");
+  lsc.writeDigital(4, "HIGH");
+
+  lsc.onEdge((pin, type) => {
+    console.log("EDGE", pin, type);
+  });
+
+  lsc.setPinMode(2, "INPUT");
+  lsc.digitalWatch(2);
+})();
