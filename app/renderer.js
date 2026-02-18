8ff68e46d393e8068408b17fdcc8a338754ac7d0
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-19 00:08:54 +0530

    implemenet the com port population for the selection

--- app/renderer.js
+++ app/renderer.js
@@ -1,2 +1,17 @@
-const information = document.getElementById("info");
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+const portSelect = document.getElementById("comPort");
+const baudRate = document.getElementById("baudRate");
+
+async function populatePorts() {
+  const ports = await window.api.getComPorts();
+
+  portSelect.innerHTML = "";
+
+  ports.forEach((p) => {
+    const option = document.createElement("option");
+    option.value = p;
+    option.text = p;
+    portSelect.appendChild(option);
+  });
+}
+
+populatePorts();
