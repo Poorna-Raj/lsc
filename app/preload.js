5fd3b375f9ce647917855197e5532f980ca66005
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-19 01:48:10 +0530

    define the connect method to initilaize the connection using the lsc client

--- app/preload.js
+++ app/preload.js
@@ -1,5 +1,14 @@
 const { contextBridge, ipcRenderer } = require("electron");
+const LSCClient = require("../lsc-client/dist/LSCClient.js").default;
+let client = null;
 
 contextBridge.exposeInMainWorld("api", {
   getComPorts: () => ipcRenderer.invoke("get-com-ports"),
+  connect: async (port, baud) => {
+    client = new LSCClient(port, baud);
+    await client.connect();
+  },
+  getClient: () => {
+    return client;
+  },
 });
