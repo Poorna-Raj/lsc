b0cb4ace63151076fdb390c32df1f7bd69f2c28c
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-19 01:48:29 +0530

    implement the connection action listener and update the status

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,9 @@
 const portSelect = document.getElementById("comPort");
 const baudRate = document.getElementById("baudRate");
 const btnPortsRefresh = document.getElementById("refreshPortsBtn");
+const btnConnect = document.getElementById("connectBtn");
+const connectStatus = document.getElementById("connStatus");
+const statusDot = document.getElementById("statusDot");
 
 async function populatePorts() {
   const ports = await window.api.getComPorts();
@@ -17,6 +20,26 @@ async function populatePorts() {
 
 btnPortsRefresh.addEventListener("click", async () => {
   populatePorts();
+});
+
+btnConnect.addEventListener("click", async () => {
+  const port = portSelect.value;
+  const baud = Number(baudRate.value);
+
+  if (!port || !baud) {
+    console.log("Undefined port or baud rate!");
+    return;
+  }
+
+  try {
+    await window.api.connect(port, baud);
+    connectStatus.textContent = "● Connected";
+    connectStatus.classList.add("connected");
+    statusDot.classList.add("status-dot--active");
+    console.log("Connected to", port, "at", baud);
+  } catch (err) {
+    console.error("Connection Failed:", err);
+  }
 });
 
 populatePorts();
